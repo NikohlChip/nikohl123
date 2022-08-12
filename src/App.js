a5b82c1043fb16777aaca48af299f055457a4957
Nikohl <war2nep@gmail.com>
2022-08-13 04:51:42 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee'
 
 function App() {
+  console.log('listing employees');
+  const showEmployees = 1;
   return (
     <div className="App">
+      {showEmployees ?
+        <Employee /> :
+        <p>unable to view</p>  
+      }
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
